d7656cd2bb9d88484d527aab1052be6a1020b8f3
Erick Alejandro Vazquez Vargas <evazquez@pai.lat>
2024-01-05 21:38:04 -0600

    feat: Update messages

--- src/controllers/users.controllers.js
+++ src/controllers/users.controllers.js
@@ -2,11 +2,11 @@ import { pool } from '../db.js';
 
 export const getUsers = async (req, res) => {
     try {
-        const [rows] = await pool.query('SELECT * FROM users');
+        const [rows] = await pool.query('SELECT * FROM users WHERE active = 1');
         res.send(rows);
     } catch (e) {
         return res.status(500).json({
-            message: 'Algo salió mal con la petición :/'
+            message: 'Algo salió mal con la petición.'
         });
     }
 }
@@ -18,14 +18,14 @@ export const getUser = async (req, res) => {
 
         if (rows.length <= 0) {
             return res.status(404).json({
-                message: 'User not found'
+                message: 'Usuario no encontrado.'
             });
         }
 
         res.json(rows[0]);
     } catch (e) {
         return res.status(500).json({
-            message: 'Algo salió mal con la petición :/'
+            message: 'Algo salió mal con la petición.'
         });
     }
 }
@@ -35,13 +35,13 @@ export const createUser = async (req, res) => {
     try {
         const [rows] = await pool.query('INSERT INTO users set ?', [data]);
         res.send({
-            message: 'User Created',
+            message: 'Usuario creado exitosamente.',
             id: rows.insertId,
             data
         });
     } catch (e) {
         return res.status(500).json({
-            message: 'Algo salió mal con la petición :/'
+            message: 'Algo salió mal con la petición.'
         });
     }
 }
@@ -54,7 +54,7 @@ export const updateUser = async (req, res) => {
 
         if (result.affectedRows <= 0) {
             return res.status(404).json({
-                message: 'User not found'
+                message: 'Usuario no encontrado.'
             });
         }
 
@@ -62,25 +62,25 @@ export const updateUser = async (req, res) => {
         res.json(rows[0]);
     } catch (e) {
         return res.status(500).json({
-            message: 'Algo salió mal con la petición :/'
+            message: 'Algo salió mal con la petición.'
         });
     }
 }
 
 export const deleteUser = async (req, res) => {
     try {
-        const [result] = await pool.query('DELETE FROM users WHERE id = ?', [req.params.id]);
+        const [result] = await pool.query('UPDATE users SET active=0 WHERE id = ?', [req.params.id]);
 
         if (result.affectedRows <= 0) {
             return res.status(404).json({
-                message: 'User not found'
+                message: 'Usuario no encontrado.'
             });
         }
 
         res.sendStatus(204);
     } catch (e) {
         return res.status(500).json({
-            message: 'Algo salió mal con la petición :/'
+            message: 'Algo salió mal con la petición.'
         });
     }
 }
