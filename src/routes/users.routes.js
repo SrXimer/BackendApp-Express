2b5994b1bc9f252c51de916c67b7875841182877
Erick Alejandro Vazquez Vargas <evazquez@pai.lat>
2024-01-07 16:28:53 -0600

    feat: Add api key detect

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,16 +1,17 @@
 import {Router} from 'express';
 import { getUsers, getUser, createUser, updateUser, deleteUser } from '../controllers/users.controllers.js';
+import { verificarApiKey } from '../verificationApiKey.js';
 
 const router = Router();
 
-router.get('/users', getUsers);
+router.get('/users', verificarApiKey, getUsers);
 
-router.get('/users/:id', getUser);
+router.get('/users/:id', verificarApiKey, getUser);
 
-router.post('/users', createUser);
+router.post('/users', verificarApiKey, createUser);
 
-router.patch('/users/:id', updateUser);
+router.patch('/users/:id', verificarApiKey, updateUser);
 
-router.delete('/users/:id', deleteUser);
+router.delete('/users/:id', verificarApiKey, deleteUser);
 
 export default router;
